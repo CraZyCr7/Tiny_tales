69487120b7f58eb419bdcc75bafa6b050af809df
Ayush Sharma <88885798+CraZyCr7@users.noreply.github.com>
2023-11-22 00:45:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.Fragment>
       <App />
-      <p dangerouslySetInnerHTML={{ __html: required }} />
     </React.Fragment>
   </React.StrictMode>,
 );
